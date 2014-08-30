ab2422991e4e315eace638bb009c9a7e5fc19b22
c5153150 <valeriy.kashpruk@sap.com>
2014-08-30 15:48:39 +0300

    timelinejs

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,5 @@
 class PublicController < ApplicationController
-  before_action :set_calendar, only: [:calendar, :pie_data, :subscribe, :unsubscribe]
+  before_action :set_calendar, only: [:calendar, :pie_data, :timelinejs, :subscribe, :unsubscribe]
 
   def index
   end
@@ -40,6 +40,40 @@ class PublicController < ApplicationController
   end
 
   def timelinejs
+    starts_at = '2014-01-01'.to_date #Event.asc(:starts_at).limit(1).first.starts_at.to_date
+    ends_at = '2015-01-01'.to_date #Event.desc(:ends_at).limit(1).first.ends_at.to_date
+
+    timeline = {
+        'timeline' => {
+          'headline'=>@calendar.title,
+          'type'=>'default',
+          'text' => @calendar.description,
+          'startDate' => '2014,01,01',
+          'asset'=>
+              {
+                  'media'=>'https=>//vine.co/v/b55LOA1dgJU',
+                  'credit'=>'credit',
+                  'caption'=>'caption'
+              },
+          'date' => []
+        }
+    }
+    @calendar.events.all.each { |e|
+      point = {}
+      point['startDate'] = e.starts_at.strftime("%Y,%m,%d")
+      point['endDate'] = e.ends_at.strftime("%Y,%m,%d") unless e.ends_at.nil?
+      point['headline'] = "#{e.holder_text} - #{e.title}"
+      point['text'] = e.description
+      point['asset'] =
+          {
+              'media'=>'https=>//vine.co/v/b55LOA1dgJU',
+              'thumbnail'=>'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRr3edmflwvOiKZFhl9BVhZTJBR7cL2scqXW11W8AT5gPu7sQW4',
+              'credit'=>'',
+              'caption'=>''
+          }
+
+      timeline['timeline']['date'] << point
+    }
 
     text =
         {
@@ -49,6 +83,12 @@ class PublicController < ApplicationController
                     'type'=>'default',
                     'text'=>'People say stuff',
                     'startDate'=>'2012,1,26',
+                    'asset'=>
+                        {
+                            'media'=>'https=>//vine.co/v/b55LOA1dgJU',
+                            'credit'=>'',
+                            'caption'=>''
+                        },
                     'date'=> [
                         {
                             'startDate'=>'2011,12,12',
@@ -58,236 +98,11 @@ class PublicController < ApplicationController
                             'asset'=>
                                 {
                                     'media'=>'https=>//vine.co/v/b55LOA1dgJU',
+                                    'thumbnail'=>'',
                                     'credit'=>'',
                                     'caption'=>''
                                 }
                         },
-                        {
-                            'startDate'=>'2012,1,26',
-                            'endDate'=>'2012,1,27',
-                            'headline'=>'Sh*t Politicians Say',
-                            'text'=>'<p>In true political fashion, his character rattles off common jargon heard from people running for office.</p>',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/u4XpeU9erbg',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,10',
-                            'headline'=>'Sh*t Nobody Says',
-                            'text'=>'<p>Have you ever heard someone say “can I burn a copy of your Nickelback CD?” or “my Bazooka gum still has flavor!” Nobody says that.</p>',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/f-x8t0JOnVw',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,26',
-                            'headline'=>'Sh*t Chicagoans Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/Ofy5gNkKGOo',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,12',
-                            'headline'=>'Sh*t Girls Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/u-yLGIH7W9Y',
-                                    'credit'=>'',
-                                    'caption'=>'Writers & Creators=> Kyle Humphrey & Graydon Sheppard'
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,4',
-                            'headline'=>'Sh*t Broke People Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/zyyalkHjSjo',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-
-                        {
-                            'startDate'=>'2012,1,4',
-                            'headline'=>'Sh*t Silicon Valley Says',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/BR8zFANeBGQ',
-                                    'credit'=>'',
-                                    'caption'=>'written, filmed, and edited by Kate Imbach & Tom Conrad'
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,25',
-                            'headline'=>'Бюджетний календар - послідовність дій',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/OmWFnd-p0Lw',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,23',
-                            'headline'=>'Sh*t Graphic Designers Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/KsT3QTmsN5Q',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,30',
-                            'headline'=>'Sh*t Wookiees Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/vJpBCzzcSgA',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,17',
-                            'headline'=>'Sh*t People Say About Sh*t People Say Videos',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/c9ehQ7vO7c0',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,20',
-                            'headline'=>'Sh*t Social Media Pros Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/eRQe-BT9g_U',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,11',
-                            'headline'=>'Sh*t Old People Say About Computers',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/HRmc5uuoUzA',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,11',
-                            'headline'=>'Sh*t College Freshmen Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/rwozXzo0MZk',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,16',
-                            'headline'=>'Sh*t Girls Say - Episode 2',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/kbovd-e-hRg',
-                                    'credit'=>'',
-                                    'caption'=>'Writers & Creators=> Kyle Humphrey & Graydon Sheppard'
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,24',
-                            'headline'=>'Sh*t Girls Say - Episode 3 Featuring Juliette Lewis',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/bDHUhT71JN8',
-                                    'credit'=>'',
-                                    'caption'=>'Writers & Creators=> Kyle Humphrey & Graydon Sheppard'
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,27',
-                            'headline'=>'Sh*t Web Designers Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/MEOb_meSHhQ',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,12',
-                            'headline'=>'Sh*t Hipsters Say',
-                            'text'=>'No meme is complete without a bit of hipster-bashing.',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/FUhrSVyu0Kw',
-                                    'credit'=>'',
-                                    'caption'=>'Written, Directed, Conceptualized and Performed by Carrie Valentine and Jessica Katz'
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,6',
-                            'headline'=>'Sh*t Cats Say',
-                            'text'=>'No meme is complete without cats. This had to happen, obviously.',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/MUX58Vi-YLg',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-                        {
-                            'startDate'=>'2012,1,21',
-                            'headline'=>'Sh*t Cyclists Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/GMCkuqL9IcM',
-                                    'credit'=>'',
-                                    'caption'=>'Video script, production, and editing by Allen Krughoff of Hardcastle Photography'
-                                }
-                        },
-                        {
-                            'startDate'=>'2011,12,30',
-                            'headline'=>'Sh*t Yogis Say',
-                            'text'=>'',
-                            'asset'=>
-                                {
-                                    'media'=>'http=>//youtu.be/IMC1_RH_b3k',
-                                    'credit'=>'',
-                                    'caption'=>''
-                                }
-                        },
-
-
-
-
                         {
                             'startDate'=>'2012,3,18',
                             'headline'=>'Sh*t New Yorkers Say',
@@ -295,6 +110,7 @@ class PublicController < ApplicationController
                             'asset'=>
                                 {
                                     'media'=>'http=>//youtu.be/yRvJylbSg7o',
+                                    'thumbnail'=>'',
                                     'credit'=>'',
                                     'caption'=>'Directed and Edited by Matt Mayer, Produced by Seth Keim, Written by Eliot Glazer. Featuring Eliot and Ilana Glazer, who are siblings, not married.'
                                 }
@@ -303,7 +119,8 @@ class PublicController < ApplicationController
                 }
         }
 
-    render json: text
+    #binding.pry
+    render json: timeline
 
   end
 
