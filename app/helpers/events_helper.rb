a1c22558ea568877bb0197e2bd99ce29da957eeb
kavaleriy <begemot.lery@mail.ru>
2014-10-25 12:40:27 -0700

    Finalize calendar

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def date_to_string(date)
+    I18n.l(date, format: '%-d %B %Y %H:%M').gsub(/00:00/, '')
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -37,7 +37,7 @@ class Event
     self.holder_text = self.holder == 1 ? 'Місто' : 'Громада'
 
     self.starts_at_string = I18n.l(self.starts_at, format: '%-d %B %Y %H:%M').gsub(/00:00/, '') unless self.starts_at.nil?
-    self.ends_at_string = I18n.l(self.ends_at, format: '%-d %B %Y %H:%M').gsub(/00:00/, '') unless self.ends_at.nil?
+    self.ends_at_string = I18n.l(self.ends_at, format: '%-d %B %Y %H:%M') unless self.ends_at.nil?
     self.all_day_string = self.all_day ? 'так' : 'ні'
   end
 
