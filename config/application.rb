89335f3060ca9cf555f36a23761600544def7fec
c5153150 <valeriy.kashpruk@sap.com>
2014-11-17 14:42:51 +0200

    Add devise

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module Budget
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :uk
+
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
