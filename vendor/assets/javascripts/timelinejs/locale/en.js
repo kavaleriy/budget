1c3792c659467bdec83bf46d1836b87d536366e0
HellLena <longlivehard.n.heavy@gmail.com>
2015-01-29 08:00:21 +0200

    JS localization

---
+++ vendor/assets/javascripts/timelinejs/locale/en.js
@@ -0,0 +1,17 @@
+/*
+    TimelineJS - ver. 2.33.1 - 2014-06-24
+    Copyright (c) 2012-2013 Northwestern University
+    a project of the Northwestern University Knight Lab, originally created by Zach Wise
+    https://github.com/NUKnightLab/TimelineJS
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+/*
+    TimelineJS - ver. 2.30.0 - 2014-02-20
+    Copyright (c) 2012-2013 Northwestern University
+    a project of the Northwestern University Knight Lab, originally created by Zach Wise
+    https://github.com/NUKnightLab/TimelineJS
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*//* Ukrainian LANGUAGE 
+================================================== */typeof VMM!="undefined"&&(VMM.Language={lang:"en",api:{wikipedia:"en"},date:{month:["January","February","March","April","May","June","July","August","September","October","November","December"],month_abbr:["Jan","Feb","March","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"],day:["Sunday","Monday","Tueasday","Wednesday","Thursday","Friday","Saturday],day_abbr:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]},dateformats:{year:"yyyy",month_short:"mmm",month:"mmmm yyyy",full_short:"d mmm",full:"d mmmm',' yyyy",time_short:"H:MM:ss",time_no_seconds_short:"H:MM",time_no_seconds_small_date:"H:MM'<br/><small>'d mmmm',' yyyy'</small>'",full_long:"d mmm yyyy 'у' H:MM",full_long_small_date:"H:MM'<br/><small>d mmm',' yyyy'</small>'"},messages:{loading_timeline:"Loading...",return_to_title:"Back to the beginning",expand_timeline:"Enlarge",contract_timeline:"Reduce",wikipedia:"Form Wikipedia, free encyclopedia",loading_content:"Loading content",loading:"Loading"}});
