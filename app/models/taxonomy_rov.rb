5cf325275d1d6b0e13707b3ac113b43637279c5b
c5153150 <valeriy.kashpruk@sap.com>
2015-02-05 18:00:01 +0200

    fix taxonomy translations

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -1,9 +1,9 @@
 class TaxonomyRov < Taxonomy
-  VERSION = 1
+  VERSION = 2
   COLUMNS = {
-      'kvk' =>{:level => 1, :title=>I18n.t('activerecord.models.taxonomy_rov.department')},
-      'ktfk'=>{:level => 2, :title=>I18n.t('activerecord.models.taxonomy_rov.func_code')},
-      'kekv' =>{:level => 3, :title=>I18n.t('activerecord.models.taxonomy_rov.economy')},
+      'kvk' =>{:level => 1, :title=>I18n.t('activerecord.taxonomy_rov.department')},
+      'ktfk'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
+      'kekv' =>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
       # 'krk'=>{:level => 4, :title=>I18n.t('activerecord.models.taxonomy_rov.disposer')},
   }
 
