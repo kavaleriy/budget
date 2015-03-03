77df52cf1c246dfb5b0fbb3a929aa2929857ca5b
c5153150 <valeriy.kashpruk@sap.com>
2015-03-03 18:47:23 +0200

    Add sankey to Module 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( )
 Rails.application.config.assets.precompile += %w( d3.geom.js jquery.history.js raphael.js vis4.js Tween.js bubbletree.js cofog.js )
+Rails.application.config.assets.precompile += %w( biPartite.js sankey.js )
