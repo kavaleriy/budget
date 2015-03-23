62db252fdeda5f12c4b919316cd3442760d2b13c
HellLena <longlivehard.n.heavy@gmail.com>
2015-03-23 14:53:52 +0200

    Saving budget files id to model Sankey

--- app/models/sankey.rb
+++ app/models/sankey.rb
@@ -1,4 +1,10 @@
 class Sankey
   include Mongoid::Document
+  #
+  # field :year, type: String
+  field :rot_file_id, type: String
+  field :rov_file_id, type: String
+
+  # validates_uniqueness_of :year, scope: [:budget_file_rot_fond, :budget_file_rov_fond]
 
 end
