067ff7ab9346feffd5647ab9bfd9da0b95336575
HellLena <longlivehard.n.heavy@gmail.com>
2015-03-25 11:09:56 +0200

    Sankey: add validate rules for model sankey.rb

--- app/models/sankey.rb
+++ app/models/sankey.rb
@@ -1,10 +1,8 @@
 class Sankey
   include Mongoid::Document
-  #
-  # field :year, type: String
+
   field :rot_file_id, type: String
   field :rov_file_id, type: String
 
-  # validates_uniqueness_of :year, scope: [:budget_file_rot_fond, :budget_file_rov_fond]
-
+  validates :rot_file_id, uniqueness: { scope: :rov_file_id, message: "Така візуалізація вже існує" }
 end
