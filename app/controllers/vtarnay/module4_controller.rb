db9201dd14ea4e5f60fd1cee9d0e7fdad4236343
c5153150 <valeriy.kashpruk@sap.com>
2015-03-28 19:53:34 +0200

    progress vtarnay-module4

--- app/controllers/vtarnay/module4_controller.rb
+++ app/controllers/vtarnay/module4_controller.rb
@@ -1,19 +1,19 @@
 class Vtarnay::Module4Controller < ApplicationController
   layout 'application_vtarnay'
 
-  before_action :set_budget_file, only: [:show]
+  before_action :set_taxonomy_file, only: [:show]
 
   def index
-    @budget_files = BudgetFile.all
+    @taxonomies = Taxonomy.all
   end
 
   def show
-    @chart = 3;
+
   end
 
   private
 
-  def set_budget_file
-    @budget_file = BudgetFile.find(params[:id])
+  def set_taxonomy_file
+    @taxonomy = Taxonomy.find(params[:id])
   end
 end
