b94107cb44396b6dbfcfba46720fa86dcdb736ae
c5153150 <valeriy.kashpruk@sap.com>
2015-03-30 08:51:39 +0300

    fix taxonomy rov

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -3,8 +3,9 @@ class TaxonomyRov < Taxonomy
   COLUMNS = {
       'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
       'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
-      'ktfk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
-      'kekv' =>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
+      'kekv' =>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
+      'ktfk_aaa'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
+      'ktfk'=>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
       # 'krk'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
   }
 
