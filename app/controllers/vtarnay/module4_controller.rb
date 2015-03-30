419911d27077e22ac5369809c8887aa307018065
kavaleriy <begemot.lery@mail.ru>
2015-03-29 21:22:09 -0700

    add vtarnay/module 4

--- app/controllers/vtarnay/module4_controller.rb
+++ app/controllers/vtarnay/module4_controller.rb
@@ -8,12 +8,22 @@ class Vtarnay::Module4Controller < ApplicationController
   end
 
   def show
-
   end
 
   private
 
   def set_taxonomy_file
     @taxonomy = Taxonomy.find(params[:id])
+
+    @sel_year = '0'
+    @sel_month = '0'
+
+    range = {}
+    @taxonomy.get_range.each{ |item| item.each{ |k, v| range[k] = v } }
+    @range = range.sort_by{|k,v| k.to_i}
+
+    @sel_year = @range.last[0]
+    @sel_month = @range.last[1].first
   end
+
 end
