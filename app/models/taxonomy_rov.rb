d06ee54cbcc9f19e5a1b3f7c750f90ac18fe331b
c5153150 <valeriy.kashpruk@sap.com>
2015-04-02 17:26:39 +0300

    change rov taxonomy  levels order

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -1,11 +1,11 @@
 class TaxonomyRov < Taxonomy
-  VERSION = 4
+  VERSION = 5
   COLUMNS = {
       'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
       'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
-      'kekv' =>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
+      'ktfk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
+      'kekv' =>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
       # 'ktfk_aaa'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
-      'ktfk'=>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
       # 'krk'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
   }
 
