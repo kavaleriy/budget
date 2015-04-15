19c98b6d3860d77bd0fbe580b80143298dda0390
kavaleriy <begemot.lery@mail.ru>
2015-04-15 14:18:41 -0700

    fix ktfk_aaa codes 81 and 91

--- app/models/budget_file_rov.rb
+++ app/models/budget_file_rov.rb
@@ -11,15 +11,20 @@ class BudgetFileRov < BudgetFile
     return if amount.nil? || amount == 0
 
     ktfk = row['KTFK'].to_s
+
+    ktfk_aaa = ktfk.slice(0, ktfk.length - 3).ljust(3, '0')
+    ktfk_aaa = '800' if ktfk_aaa == '810'
+    ktfk_aaa = '900' if ktfk_aaa == '910'
+
     {
         '_year' => row['DATA'].to_date.year.to_s,
         '_month' => row['MONTH'].to_s.split('.')[0],
         'amount' => amount / 100,
+        'ktfk' => ktfk,
+        'ktfk_aaa' => ktfk_aaa,
         'fond' => row['KKFN'].to_s,
         'kvk' => row['KVK'].to_s,
         'kekv' => row['KEKV'].to_s,
-        'ktfk' => ktfk,
-        'ktfk_aaa' => ktfk.slice(0, ktfk.length - 3),
         'krk' => row['KRK'].to_s,
     }
   end
