348a87ee341ac75b5c808c5ee6aabc33855a0508
c5153150 <valeriy.kashpruk@sap.com>
2015-04-16 16:20:27 +0300

    update Rov columns

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -6,7 +6,7 @@ class TaxonomyRov < Taxonomy
       {
           'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
           'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
-          'krk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
+          # 'krk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
           # 'ktfk_aaa'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
           'ktfk'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
           'kekv' =>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
@@ -16,7 +16,7 @@ class TaxonomyRov < Taxonomy
           'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
           # 'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
           'ktfk_aaa'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
-          'krk'=>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
+          # 'krk'=>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
           'ktfk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
           'kekv' =>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
       }
