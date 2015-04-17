d269034384a9e8e0912a611d6fe29b0ea9a2852b
c5153150 <valeriy.kashpruk@sap.com>
2015-04-17 16:45:42 +0300

    remove fond level from taxonomy

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -4,21 +4,19 @@ class TaxonomyRov < Taxonomy
 
     if is_kvk
       {
-          'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
-          'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
-          # 'krk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
+          'kvk' =>{:level => 1, :title=>I18n.t('activerecord.taxonomy_rov.department')},
+          # 'krk'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
           # 'ktfk_aaa'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
-          'ktfk'=>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
-          'kekv' =>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
+          'ktfk'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
+          'kekv' =>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
       }
     else
       {
-          'fond'=>{:level => 1, :title=> I18n.t('activerecord.taxonomy_rot.fond')},
           # 'kvk' =>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.department')},
-          'ktfk_aaa'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
+          'ktfk_aaa'=>{:level => 1, :title=>I18n.t('activerecord.taxonomy_rov.func_code_aaa')},
           # 'krk'=>{:level => 5, :title=>I18n.t('activerecord.taxonomy_rov.disposer')},
-          'ktfk'=>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
-          'kekv' =>{:level => 4, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
+          'ktfk'=>{:level => 2, :title=>I18n.t('activerecord.taxonomy_rov.func_code')},
+          'kekv' =>{:level => 3, :title=>I18n.t('activerecord.taxonomy_rov.economy')},
       }
     end
 
