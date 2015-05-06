ba9d64b4f7da950a49dd3afcdd52cc6cf0c4c622
c5153150 <valeriy.kashpruk@sap.com>
2015-05-06 17:30:25 +0300

    add current user view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,17 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => t('application.auth_error') + "#{exception.message}"
   end
 
+
+  # deals with mass assignment
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :town) }
+  end
+
   private
 
   def cancan_hack
