460b187df4fdc52a514e474d688e781f209f4905
HellLena <longlivehard.n.heavy@gmail.com>
2015-05-21 09:02:07 +0300

    fix loading budget files ROT plan-fact

--- app/models/budget_file_rot_planfact.rb
+++ app/models/budget_file_rot_planfact.rb
@@ -10,7 +10,7 @@ class BudgetFileRotPlanfact < BudgetFile
     amount_plan = row['План'].to_i
     amount_fact = row['Факт'].to_i
 
-    kkd = row['Код'].to_s.split('.')[0]
+    kkd = row['Код'].to_s.gsub(/\s+/, "").split('.')[0]
     return if kkd.to_i == 0
 
     fond = row['Фонд'].to_s.split('.')[0]
@@ -36,6 +36,7 @@ class BudgetFileRotPlanfact < BudgetFile
       [{t: 'kkd_a', key: kkd.slice(0, 1)}, {t: 'kkd_bb', key: kkd.slice(0, 3)}, {t: 'kkd_cc', key: kkd.slice(0, 5)}, {t: 'kkd', key: kkd.slice(0, 8)}].map { |v|
         item[v[:t]] = v[:key]
       }
+
       item
     }.reject {|c| c.nil? || c['amount'] == 0 }
   end
