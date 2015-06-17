06148017f4aa75cd8826a17a0b388428eb2168f7
HellLena <longlivehard.n.heavy@gmail.com>
2015-06-17 10:05:30 +0300

    export calendar to csv

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
-
+Mime::Type.register_alias "text/excel", :xls
 # Add indicator_file mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
