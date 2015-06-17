42cd5506a95dc5899a32259a86f840bc3b92bfb3
HellLena <longlivehard.n.heavy@gmail.com>
2015-06-17 11:11:27 +0300

    import table from programs expences file

--- app/models/programs/expences_file.rb
+++ app/models/programs/expences_file.rb
@@ -6,6 +6,7 @@ class Programs::ExpencesFile
   field :author, type: String
   field :title, type: String
   field :description, type: String
+  field :expences, type: Hash
 
   belongs_to :programs_target_program, :class_name => 'Programs::TargetProgram', autosave: true
 
@@ -13,5 +14,64 @@ class Programs::ExpencesFile
   skip_callback :update, :before, :store_previous_model_for_expences_file
 
   validates_presence_of :expences_file, message: I18n.t('form.choose_file')
+
+  def import table
+
+    self.expences = {}
+
+    table[:rows].each{|row|
+      if row['phase'].blank? && row['year'].blank?
+        self.expences['total'] = {} if self.expences['total'].nil?
+        if row['sources'].blank?
+          self.expences['total']['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['total']['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['total']['description'] = row['description'] unless row['description'].blank?
+        else
+          self.expences['total']['sources'] = {} if self.expences['total']['sources'].blank?
+          source = row['sources'].to_i.to_s
+          self.expences['total']['sources'][source] = {} if self.expences['total']['sources'][source].blank?
+          self.expences['total']['sources'][source]['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['total']['sources'][source]['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['total']['sources'][source]['description'] = row['description'] unless row['description'].blank?
+        end
+      elsif row['year'].blank?
+        phase = row['phase'].to_i.to_s
+        self.expences['phases'] = {} if self.expences['phases'].blank?
+        self.expences['phases'][phase] = {} if self.expences['phases'][phase].blank?
+        self.expences['phases'][phase]['phase_start'] = {} unless row['phase_start'].blank?
+        self.expences['phases'][phase]['phase_end'] = {} unless row['phase_end'].blank?
+        if row['sources'].blank?
+          self.expences['phases'][phase]['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['phases'][phase]['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['phases'][phase]['description'] = row['description'] unless row['description'].blank?
+        else
+          source = row['sources'].to_i.to_s
+          self.expences['phases'][phase]['sources'] = {} if self.expences['phases'][phase]['sources'].blank?
+          self.expences['phases'][phase]['sources'][source] = {} if self.expences['phases'][phase]['sources'][source].blank?
+          self.expences['phases'][phase]['sources'][source]['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['phases'][phase]['sources'][source]['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['phases'][phase]['sources'][source]['description'] = row['description'] unless row['description'].blank?
+        end
+      else
+        year = row['year'].to_i.to_s
+        self.expences['years'] = {} if self.expences['years'].blank?
+        self.expences['years'][year] = {} if self.expences['years'][year].blank?
+        if row['sources'].blank?
+          self.expences['years'][year]['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['years'][year]['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['years'][year]['description'] = row['description'] unless row['description'].blank?
+        else
+          source = row['sources'].to_i.to_s
+          self.expences['years'][year]['sources'] = {} if self.expences['years'][year]['sources'].blank?
+          self.expences['years'][year]['sources'][source] = {} if self.expences['years'][year]['sources'][source].blank?
+          self.expences['years'][year]['sources'][source]['amount_plan'] = row['amount_plan'] unless row['amount_plan'].blank?
+          self.expences['years'][year]['sources'][source]['amount_fact'] = row['amount_fact'] unless row['amount_fact'].blank?
+          self.expences['years'][year]['sources'][source]['description'] = row['description'] unless row['description'].blank?
+        end
+      end
+
+    }
+
+  end
 
 end
