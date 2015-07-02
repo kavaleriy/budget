e29e350137360bdb26b0b62450f565e868aaec1a
c5153150 <valeriy.kashpruk@sap.com>
2015-07-02 15:34:47 +0300

    fix documentation category ajax

--- app/models/repairing/geojson_builder.rb
+++ app/models/repairing/geojson_builder.rb
@@ -1,10 +1,10 @@
 class Repairing::GeojsonBuilder
 
   def self.build_repair(repair)
-    if repair[:address_to].blank?
-      build_repair_point(repair)
-    else
+    if repair[:coordinates][0].is_a?(Array)
       build_repair_path(repair)
+    else
+      build_repair_point(repair)
     end
   end
 
