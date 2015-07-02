57fa58fb0aa0c9982590793300f66f4985b70492
c5153150 <valeriy.kashpruk@sap.com>
2015-07-02 17:02:11 +0300

    fix heroku

---
+++ app/models/taxonomy_rov_fond.rb
@@ -0,0 +1,27 @@
+class TaxonomyRovFond < Taxonomy
+
+  def columns
+
+    if is_kvk
+      {
+          # 'fond'=>{:level => 1, :title=> I18n.t('mongoid.taxonomy_rot.fond')},
+          'kvk' =>{:level => 1, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.department')},
+          # 'krk'=>{:level => 2, :title=>I18n.t('mongoid.taxonomy_rov.disposer')},
+          # 'ktfk_aaa'=>{:level => 2, :title=>I18n.t('mongoid.taxonomy_rov.func_code_aaa')},
+          'ktfk'=>{:level => 2, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.func_code')},
+          'kekv' =>{:level => 3, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.economy')},
+      }
+    else
+      {
+          # 'fond'=>{:level => 1, :title=> I18n.t('mongoid.taxonomy_rot.fond')},
+          # 'kvk' =>{:level => 2, :title=>I18n.t('mongoid.taxonomy_rov.department')},
+          'ktfk_aaa'=>{:level => 2, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.func_code_aaa')},
+          # 'krk'=>{:level => 5, :title=>I18n.t('mongoid.taxonomy_rov.disposer')},
+          'ktfk'=>{:level => 3, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.func_code')},
+          'kekv' =>{:level => 4, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.economy')},
+      }
+    end
+
+  end
+
+end
