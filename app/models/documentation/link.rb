dc09c87975ce5d5f12cf4b719b9e99f6db0e345a
gmarysa <longlivehard.n.heavy@gmail.com>
2015-09-09 10:44:43 +0300

    links years and towns, html5 editor to event

--- app/models/documentation/link.rb
+++ app/models/documentation/link.rb
@@ -2,9 +2,10 @@ class Documentation::Link
   include Mongoid::Document
 
   belongs_to :link_category, class_name: 'Documentation::LinkCategory', autosave: true
-
+  belongs_to :town
   field :title, type: String
   field :value, type: String
-
+  field :yearFrom, type: Integer
+  field :yearTo, type: Integer
 
 end
