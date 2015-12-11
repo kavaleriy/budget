00d6783a1c63d286466b7db68c361703f2b2bbfb
Sorey <3sorey4@gmail.com>
2015-12-11 11:32:27 +0200

    start create upload book

--- app/models/library/book.rb
+++ app/models/library/book.rb
@@ -1,8 +1,27 @@
 class Library::Book
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :title, type: String
   # field :year_published, type: String
   #field :link, type: String
   field :description, type: String
 
+
+  mount_uploader :doc_file, DocumentationUploader
+  skip_callback :update, :before, :store_previous_model_for_doc_file
+
+  validates_presence_of :doc_file, message: 'Потрібно вибрати Файл'
+  validates :doc_file,
+            :presence => true,
+            :file_size => {
+                :maximum => 11.megabytes.to_i
+            }
+  #
+  # before_save :generate_title
+  #
+  # private
+  #
+  # def generate_title
+  #   self.title = self.doc_file_identifier unless self.title?
+  # end
 end
