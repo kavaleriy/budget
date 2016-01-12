50d78d9e4b201d0683fcbce31c0ecd2148936420
Sorey <3sorey4@gmail.com>
2016-01-12 09:44:17 +0200

    Finished category-filter for books

--- app/models/library/book.rb
+++ app/models/library/book.rb
@@ -12,7 +12,6 @@ class Library::Book
   field :description, type: String
   field :book_url, type: String
 
-  # attr_accessor :category
   belongs_to :owner, class_name: 'User'
 
   validates :title, :presence => true
@@ -23,30 +22,11 @@ class Library::Book
                 :maximum => 50.megabytes.to_i, message: 'Максимально-можливий розмір файлу - 50 мб.'
             }
 
-  scope :category, -> { select(where(author: true)).distinct }
-
-
-  scope :active, -> { where category: "" }
-  scope :inactive, -> { where category: nil }
-
-  def self.category
-    self.categories
-  end
   private
     mount_uploader :book_img, ImgBookUploader
     skip_callback :update, :before, :store_previous_model_for_book_img
 
     mount_uploader :book_file, BookUploader
     skip_callback :update, :before, :store_previous_model_for_book_file
-
-    def self.categories
-      # @categories = Library::Book.select('author')  #.distinct
-      self.distinct.pluck(:author)
-    end
-
-    # def self.categories
-    #   @categories = Library::Book.all(:select => "DISTINCT author")  #.distinct
-    # end
-
 
 end
