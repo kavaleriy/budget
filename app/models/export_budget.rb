8cb68f256691a95fb3ff6a922d232e1bc42f8405
HellLena <longlivehard.n.heavy@gmail.com>
2016-01-12 16:23:53 +0200

    Fix export to pdf

--- app/models/export_budget.rb
+++ app/models/export_budget.rb
@@ -1,9 +1,10 @@
 class ExportBudget
   include Mongoid::Document
   has_one :calendar
-  belongs_to :town, autosave: true
+  belongs_to :town
 
   field :year, type: Integer
   field :title, type: String
   field :template, type: String
+  field :town_id, type: String
 end
