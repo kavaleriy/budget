8e7eab784a1b5a341ccb44e6b2072f6fdc678fd9
Quicks <1989alpan@gmail.com>
2016-02-02 11:46:54 +0200

    add image field validation for budget_news

--- app/models/modules/budget_news.rb
+++ app/models/modules/budget_news.rb
@@ -1,14 +1,23 @@
 class Modules::BudgetNews
   include Mongoid::Document
+  require 'carrierwave/mongoid'
 
-  validates :title,:news_text,:link,:img,:news_date, presence: true
+  validates :title,:news_text,:link,:news_date, presence: true
   validates :link, format: { with: /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/,
                              message: I18n.t('activerecord.attributes.invalid.link') }
 
+  mount_uploader :img, BudgetNewsImageUploader
   field :title, type: String
   field :news_text, type: String
   field :link, type: String
-  field :img, type: String
+  # field :img, type: String
   field :news_date, type: Date
 
+  def delete_image_file!
+    self.remove_img!
+  end
+
+  private
+    mount_uploader :img, BudgetNewsImageUploader
+    skip_callback :update, :before, :store_previous_model_for_img
 end
