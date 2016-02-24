6607aa4384fe76285832899fd19075748ff7bd2d
Quicks <1989alpan@gmail.com>
2016-02-24 15:39:31 +0200

    Add constant and getters in calendar_action

--- app/models/calendar_action.rb
+++ app/models/calendar_action.rb
@@ -1,6 +1,11 @@
 class CalendarAction
   include Mongoid::Document
 
+  ACTION_TYPE_FOLDING = 1
+  ACTION_TYPE_ANALYSIS = 2
+  ACTION_TYPE_DISCUSSION = 3
+  ACTION_TYPE_EXECUTION = 4
+
   field :holder, type: Integer
   field :title, type: String
   field :icon, type: String
@@ -13,6 +18,22 @@ class CalendarAction
 
   validates_presence_of :holder, message: I18n.t('errors.messages.empty')
   validates_presence_of :title, message: I18n.t('errors.messages.empty')
+
+  def get_folding_type
+    ACTION_TYPE_FOLDING
+  end
+
+  def get_analysis_type
+    ACTION_TYPE_ANALYSIS
+  end
+
+  def get_discusion_type
+    ACTION_TYPE_DISCUSSION
+  end
+
+  def get_execution_type
+    ACTION_TYPE_EXECUTION
+  end
 
   # attr_accessor :holder_string
   # after_initialize :do_after_initialize
