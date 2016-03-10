3079cda0f7e3b8d27d971ea091d66070fcebb85f
Alex <1989alpan@gmail.com>
2016-03-10 15:30:26 +0200

    Fix content show

--- app/helpers/content_manager/page_containers_helper.rb
+++ app/helpers/content_manager/page_containers_helper.rb
@@ -2,4 +2,12 @@ module ContentManager::PageContainersHelper
   def get_menu_list
     ContentManager::PageContainer.get_menu_list
   end
+
+  def get_header_by_locale(content)
+    content[params[:locale]][:header]
+  end
+
+  def get_content_by_locale(content)
+    content[params[:locale]][:content]
+  end
 end
