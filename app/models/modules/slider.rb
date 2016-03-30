ff80fcdee84c63741ca020216ee0f39ea2ae7e79
Alex <1989alpan@gmail.com>
2016-03-30 16:47:24 +0300

    Config crop

--- app/models/modules/slider.rb
+++ app/models/modules/slider.rb
@@ -1,7 +1,8 @@
 require 'carrierwave/mongoid'
 class Modules::Slider
-
   include Mongoid::Document
+
+
   validates :text,:img, presence: true
   validates :sl_order, uniqueness: true
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
@@ -14,11 +15,12 @@ class Modules::Slider
   skip_callback :update, :store_previous_model_for_img
 
   def profile_geometry
-    img = MiniMagick::Image.open(self.img.slider.path)
+    img = MiniMagick::Image.open(self.img.path)
     @geometry = {:width => img[:width], :height => img[:height] }
   end
 
   def delete_image_file!
+    binding.pry
     self.remove_img!;
   end
 end
