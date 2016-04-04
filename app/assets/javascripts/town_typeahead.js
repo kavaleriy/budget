0a6c8f08c659ddcd0564b67fb4184554a339bf46
Alex <1989alpan@gmail.com>
2016-04-04 14:57:39 +0300

    brought town typeahead script in separate file

---
+++ app/assets/javascripts/town_typeahead.js
@@ -0,0 +1,20 @@
+/**
+ * Created by alex on 04.04.16.
+ */
+function init_town_typeahead(url){
+    var typeahead_count = 3;
+    $('#user_town').typeahead({
+        minLength: typeahead_count,
+        source: function (query, process) {
+            return $.get(url, { query: $('#user_town').val() }, function (json) {
+                var data = [];
+                $.each(json,function(i,name){
+                    data.push(name.text);
+                });
+                return process(data)
+            },'json');
+        }, highlighter: function(item) {
+            return item;
+        }
+    });
+}
