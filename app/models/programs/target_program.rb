151a6e990937788ad0c15807cad8eb6739a95604
Quicks <1989alpan@gmail.com>
2016-06-16 17:09:25 +0300

    add create program action

--- app/models/programs/target_program.rb
+++ app/models/programs/target_program.rb
@@ -9,7 +9,6 @@ class Programs::TargetProgram
   has_many :sub_programs,class_name: 'Programs::TargetProgram',foreign_key: 'p_id'
   has_many :indicators,class_name: 'Programs::Indicator'
 
-
-
   scope :get_main_programs,-> {where(p_id: nil)}
+  validates :title,:responsible,:description,presence: true
 end
