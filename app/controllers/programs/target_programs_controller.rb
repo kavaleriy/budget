e7cfa36c7c524c55044fef2891b3085464dae934
Quicks <1989alpan@gmail.com>
2016-06-16 17:27:39 +0300

    Update action in target program

--- app/controllers/programs/target_programs_controller.rb
+++ app/controllers/programs/target_programs_controller.rb
@@ -2,6 +2,7 @@ class Programs::TargetProgramsController < ApplicationController
   layout 'application_admin', except: [:index, :show]
   respond_to :html
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_target_program,only: [:edit,:show,:update]
   # load_and_authorize_resource
 
   # GET /programs/target_programs
@@ -27,15 +28,24 @@ class Programs::TargetProgramsController < ApplicationController
 
   def show
     stub_data
-    @program = Programs::TargetProgram.first
+    # @program = Programs::TargetProgram.first
   end
 
+  def edit
+  end
 
+  def update
+    @program.update(programs_target_program_params)
+    respond_with(@program)
+  end
 
 
 
   private
 
+  def set_target_program
+    @program = Programs::TargetProgram.find(params[:id])
+  end
   def stub_data
     @year = Date.today.year.to_s
   end
