28563baa89f2e8eee21eea6ba2cef1c47059ae70
Quicks <1989alpan@gmail.com>
2016-06-22 15:54:26 +0300

    Minor change

--- app/helpers/programs/target_programs_helper.rb
+++ app/helpers/programs/target_programs_helper.rb
@@ -5,7 +5,7 @@ module Programs::TargetProgramsHelper
 
   def calc_percentage(plan,fact)
     if fact.eql?(0)
-      -#Test some data
+      #Test some data
       22
     else
       ((fact.to_f/plan.to_f) * 100)
