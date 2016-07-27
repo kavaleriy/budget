5939c523a7bcf1fc553e5e00a2dd7b6a1704b675
Sorey <3sorey4@gmail.com>
2016-07-27 11:28:01 +0300

    revision get image and url for book

--- app/helpers/library/books_helper.rb
+++ app/helpers/library/books_helper.rb
@@ -1,2 +1,23 @@
 module Library::BooksHelper
+  def get_img book
+    if book.book_img?
+      image_tag(book.book_img.thumb)
+    else
+      image_tag("book_off.png")
+    end
+  end
+
+  def get_url book
+    if book.book_file?
+      book.book_file.url
+    elsif book.book_url?
+      book.book_url
+    end
+  end
+
+  def access_user book
+    unless current_user.nil?
+     current_user.id == book.owner_id || current_user.has_role?(:admin)
+   end
+  end
 end
