aa354434219eb6dff3e93c8c8dc8fe6365fbc54b
Sorey <3sorey4@gmail.com>
2016-07-27 13:22:24 +0300

    transfer logic book-filters to helper method

--- app/helpers/library/books_helper.rb
+++ app/helpers/library/books_helper.rb
@@ -20,4 +20,23 @@ module Library::BooksHelper
      current_user.id == book.owner_id || current_user.has_role?(:admin)
    end
   end
+
+  def book_filters
+    book_filters = []
+    # all books
+    book_filters.push({title: "Усі книги", url: library_books_path})
+    # books without category
+    categories_nil = Library::Book.pluck(category: nil).uniq
+    categories = Library::Book.pluck(:category).uniq.compact.sort   # .compact - delete all nil elements
+    if categories[0] == '' || categories_nil[0] == nil
+      book_filters.push({title: "Без категорії", url: library_books_path(category_empty: "empty")})
+    end
+    # books with category
+    categories.each do |category|
+      if category != ''
+        book_filters.push({title: category, url: library_books_path(category: category)})
+      end
+    end
+    book_filters
+  end
 end
