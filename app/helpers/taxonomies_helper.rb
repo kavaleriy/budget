e215902128b60b031ac96e8e66790221e4657035
Dmytro Herasymuk <djadja.grisha@gmail.com>
2016-08-04 12:19:29 +0300

    Add field 'active' to taxonomy model. Relate it with controller and views. Add translate.

--- app/helpers/taxonomies_helper.rb
+++ app/helpers/taxonomies_helper.rb
@@ -6,4 +6,18 @@ module TaxonomiesHelper
       '-'
     end
   end
+
+  def get_active_icon(active_fild)
+    if active_fild
+      icon = "<i class='fa fa-check-square-o fa-lg'>"
+    else
+      icon = "<i class='fa fa-square-o fa-lg'>"
+    end
+    # if active_fild.nil? || !active_fild
+    #   "<i class='fa fa-square-o fa-lg'>"
+    # else
+    #   "<i class='fa fa-check-square-o fa-lg'>"
+    # end
+    icon.html_safe
+  end
 end
