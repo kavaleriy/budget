b5d93a6d98fe8eea388e20e2e40ca99f73b1d6f8
Andrii Nizinkovskyi <enemy.vin@gmail.com>
2016-08-12 01:07:54 +0300

    clear code

--- config/routes.rb
+++ config/routes.rb
@@ -20,11 +20,6 @@ Rails.application.routes.draw do
   get 'template/load/:partial_name' => 'template#load',as: 'template_load'
 
   get '/info/:alias/show' => 'public/home#show_pages',as: 'show_pages'
-  get '/demo_index/' => 'public/home#demo_index',as: 'demo' # Delete it route after upgrade new design
-  get '/demo_profile' => 'public/home#demo_profile', as: 'demo_profile' # Delete it route after upgrade new design
-  get '/demo_repair_roads_map' => 'public/home#demo_repair_roads_map', as: 'demo_repair_roads_map' # Delete it route after upgrade new design
-  get '/demo_news' => 'public/home#demo_news', as: 'demo_news' # Delete it route after upgrade new design
-  get '/demo_news_item' => 'public/home#demo_news_item', as: 'demo_news_item' # Delete it route after upgrade new design
 
   namespace :content_manager do
     resources :page_containers
