c6a363f496f3509ca75b59b93456e5708128f1a3
Sorey <3sorey4@gmail.com>
2016-08-15 15:22:19 +0300

    design in admin

--- app/uploaders/banner_image_uploader.rb
+++ app/uploaders/banner_image_uploader.rb
@@ -31,7 +31,7 @@ class BannerImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [320, -1]
+    process resize_to_fit: [360, -1]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
