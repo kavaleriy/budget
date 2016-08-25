2be59304372e46f5d3eb481505a34293ee6a14f1
kavaleriy <begemot.lery@mail.ru>
2016-08-25 22:55:53 +0300

    fix rov-files  special fond upload logic

--- app/models/budget_file_rov.rb
+++ app/models/budget_file_rov.rb
@@ -3,9 +3,6 @@ class BudgetFileRov < BudgetFile
   protected
 
   def readline row
-    month = row['MONTH'].to_s.split('.')[0]
-    return if month == '0' # add annual amount manually
-    
     ktfk = row['KTFK'].to_i.to_s.gsub(/^0*/, "")
 
     amount = row['SUMM'].to_i
@@ -20,15 +17,17 @@ class BudgetFileRov < BudgetFile
     kod =  row['KOD'].to_s.split('.')[0]
     return if fond != '1' and kod == '1'
 
-
+    months = [ row['MONTH'].to_s.split('.')[0] ]
+    months << '0' if fond == '7' # add annual amount manually
+    
     ktfk_aaa = ktfk.slice(0, ktfk.length - 3) #.ljust(3, '0')
     ktfk_aaa = '80' if ktfk_aaa == '81'
     ktfk_aaa = '90' if ktfk_aaa == '91'
     
-    ['0', month].map { |_month|
+    months.map { |month|
       {
         '_year' => row['DATA'].to_date.year.to_s.split('.')[0],
-        '_month' => _month,
+        '_month' => month,
 
         'fond' => fond,
 
