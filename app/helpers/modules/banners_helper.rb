2b2428d78636fe249a6ff7743b0a20afd00beeb2
Sorey <3sorey4@gmail.com>
2016-08-30 12:05:53 +0300

    setting banner with url

--- app/helpers/modules/banners_helper.rb
+++ app/helpers/modules/banners_helper.rb
@@ -7,4 +7,16 @@ module Modules::BannersHelper
         banners[i+1].nil? ? banners.first : banners[i+1]
     end
   end
+
+  def get_banner_img banner
+    image_tag(banner.banner_img.thumb, :title => banner.title, class: 'img-responsive')
+  end
+
+  def get_banner banner
+    if banner.banner_url?
+      link_to get_banner_img(banner), banner.banner_url, target: '_blank'
+    else
+      get_banner_img(banner)
+    end
+  end
 end
