baaf0c11b2d6e6596a82777560f31b97e63de0b3
Sorey <3sorey4@gmail.com>
2016-08-30 11:17:48 +0300

    fix link in budget_news on banners

--- app/helpers/modules/banners_helper.rb
+++ app/helpers/modules/banners_helper.rb
@@ -1,12 +1,4 @@
 module Modules::BannersHelper
-  def get_img(banner)
-    if banner.banner_img?
-      banner.banner_img.thumb
-    else
-      banner.banner_url
-    end
-  end
-
   def get_banner_id(banners, i, move)
     case move
       when 'left'
