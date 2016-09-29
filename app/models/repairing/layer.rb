eb7ee4f301830ffe68759031f3006dd512d3d703
Quicks <1989alpan@gmail.com>
2016-09-29 16:58:27 +0300

    change request for old mongiod version

--- app/models/repairing/layer.rb
+++ app/models/repairing/layer.rb
@@ -35,4 +35,69 @@ class Repairing::Layer
     geo_json.compact
 
   end
+
+  def self.valid_layers_with_repairs
+    # TODO cache each layer if document would be larger than 16 mb
+    # this function return BSON::Document
+    # example
+    # layer hash as key
+
+    # [{"_id"=>BSON::ObjectId('56fe442467cb7d0724000003'), "repairing_category_id"=>BSON::ObjectId('560ce9576b61730991140000'), "town_id"=>BSON::ObjectId('55a818d06b617309df652500')},
+
+    # array of repairs as value
+
+    #  [{"_id"=>BSON::ObjectId('56fe442467cb7d0724000004'), "repair_date"=>2015-01-01 00:00:00 UTC, "coordinates"=>[49.8571335, 24.0187616], "layer_id"=>BSON::ObjectId('56fe442467cb7d0724000003')},
+    #     {"_id"=>BSON::ObjectId('56fe442567cb7d0724000005'),
+    #      "repair_date"=>2015-01-01 00:00:00 UTC,
+    # "coordinates"=>[["49.84698", "24.01545"], ["49.84701", "24.0157"], ["49.84757", "24.01549"], ["49.84762", "24.01548"]],
+    #     "layer_id"=>BSON::ObjectId('56fe442467cb7d0724000003')}
+
+    # find all layers where is set town and get repair category and town
+    layers = Repairing::Layer.collection.aggregate([
+                                                       {
+                                                           '$match' => {
+                                                               'town_id' => {'$ne' => nil}
+                                                           }
+                                                       },
+                                                       {
+                                                           '$project' => {
+                                                               'repairing_category_id' => 1,
+                                                               'town_id' => 1,
+                                                           }
+                                                       }
+                                                   ])
+    # get layers ids
+    layers_ids = layers.map{ |layer| layer['_id'] }
+
+    layers_with_repairs = Repairing::Repair.collection.aggregate([
+                                                                     {
+                                                                         # filter documents
+                                                                         '$match' =>{
+                                                                             '$and' =>
+                                                                                 [
+                                                                                     {
+                                                                                         # check if repair have layer
+                                                                                         layer_id: {'$in' => layers_ids },
+                                                                                         # check repair coordinates
+                                                                                         coordinates: {'$ne' => nil},
+                                                                                     }
+                                                                                 ]
+                                                                         }
+                                                                     },
+                                                                     {
+                                                                         # show this fields
+                                                                         '$project' => {
+                                                                             'coordinates' => 1,
+                                                                             'layer_id' => 1,
+                                                                             'repair_date' => 1,
+                                                                         }
+                                                                     }
+
+                                                                 ])
+    # transform repairs key to layer hash and remove layer hash from layers array
+    # group repairs by layer
+    layers_with_repairs..group_by{ |rep| rep['layer_id'] }
+    layers_with_repairs.transform_keys!{ |key| (layers.dup - layers.delete_if{ |layer| layer['_id'] == key}).first }
+  end
+
 end
