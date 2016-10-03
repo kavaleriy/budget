f43f7d96ac144e135fb9091593a6abec3a241134
Sorey <3sorey4@gmail.com>
2016-10-03 16:34:30 +0300

    simple code and code convention for repair categories

--- app/models/repairing/category.rb
+++ app/models/repairing/category.rb
@@ -1,14 +1,14 @@
 class Repairing::Category
   include Mongoid::Document
 
-  default_scope lambda { order_by(:position => :asc) }
-  scope :by_locale, lambda { where( locale: I18n.locale ) }
-  scope :tree_root, lambda { where( :category_id.in =>[ nil, '#']) }
-  scope :tree, lambda { |category_id| where( :category_id => category_id) }
+  default_scope lambda { order_by(position: :asc) }
+  scope :by_locale, lambda { where(locale: I18n.locale) }
+  scope :tree_root, lambda { where(:category_id.in => [ nil, '#']) }
+  scope :tree, lambda { |category_id| where( category_id: category_id) }
 
-  has_many :repairing_layers, :class_name => 'Repairing::Layer', autosave: true, :dependent => :nullify
-  has_many :repairing_repairs, :class_name => 'Repairing::Repair', autosave: true, :dependent => :nullify
-  has_one :parent, class_name: 'Repairing::Category', :dependent => :nullify
+  has_many :repairing_layers, class_name: 'Repairing::Layer', autosave: true, dependent: :nullify
+  has_many :repairing_repairs, class_name: 'Repairing::Repair', autosave: true, dependent: :nullify
+  has_one :parent, class_name: 'Repairing::Category', dependent: :nullify
   belongs_to :category, class_name: 'Repairing::Category'
 
   field :title, type: String
@@ -22,7 +22,7 @@ class Repairing::Category
   skip_callback :update, :before, :store_previous_model_for_img
 
   def self.tree_root
-    Repairing::Category.where( :category_id.in =>[ nil, '#']).by_locale
+    Repairing::Category.where(:category_id.in =>[ nil, '#']).by_locale
   end
 
   def childrens
