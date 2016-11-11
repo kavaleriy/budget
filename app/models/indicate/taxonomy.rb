1c4f57bed9b2366c51a0e379f84e405960fd14d4
Sorey <3sorey4@gmail.com>
2016-11-11 18:13:15 +0200

    fix show chart with old data, setting view charts

--- app/models/indicate/taxonomy.rb
+++ app/models/indicate/taxonomy.rb
@@ -34,21 +34,23 @@ class Indicate::Taxonomy
         # indicators[group][name]['id'] = replace_space_indicator_chart_id(group, name)
 
         matching = indicator['matching']
-        matching = 'fact' if matching.eql?('')
+        matching = 'fact' if matching.eql?('') || matching.nil?
         indicators[group][name][matching] = {} if indicators[group][name][matching].nil?
         indicators[group][name][matching][year] = {} if indicators[group][name][matching][year].nil?
         indicators[group][name][matching][year]['comment'] = indicator['comment']
         indicators[group][name][matching][year]['value'] = indicator['value']
         indicators[group][name][matching][year]['link'] = indicator['link']
         indicators[group][name]['id'] = replace_space_indicator_chart_id(group, name)
+        puts(indicators[group][name]['id'])
       }
     }
+    # binding.pry
     indicators
   end
 
   def replace_space_indicator_chart_id(str1, str2)
     string = "#{str1}_#{str2}"
-    string.tr(' ','_')
+    string.tr(', ','__')
   end
 
 end
