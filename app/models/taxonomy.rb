5eb98b816f0205b5b668de84f235ece848235525
Quicks <1989alpan@gmail.com>
2016-11-14 17:16:10 +0200

    change behavior for zip_budget_file_controller

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -96,7 +96,7 @@
                 if user.has_role? :admin
                   self.all
                 else
-                  self.where(:owner => user.town,:author.in => [user,nil])
+                  self.where(:town => user.town_model,:author.in => [user,nil])
                 end
               else
                 self.where(:owner => '')
