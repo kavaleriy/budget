62a6f743c41c1f042df1bb1df81d42f09b6257c4
Andrii Nizinkovskyi <enemy.vin@gmail.com>
2016-12-02 14:31:55 +0200

    Merge branch 'master' of https://github.com/kavaleriy/budget into dev_week_47
    # Conflicts:
    #	app/controllers/budget_files_controller.rb
    #	app/views/budget_files/_form.html.haml

--- app/controllers/budget_files_controller.rb
+++ app/controllers/budget_files_controller.rb
@@ -5,6 +5,10 @@ class BudgetFilesController < ApplicationController
   helper_method :sort_column, :sort_direction
   before_action :set_budget_file, only: [:show, :edit, :update, :destroy, :download]
 
+  before_action :generate_budget_file, only: [:new]
+
+  # before_action :update_user_town, only: [:create]
+
   before_action :authenticate_user!
   load_and_authorize_resource
 
@@ -46,27 +50,14 @@ class BudgetFilesController < ApplicationController
   end
 
   def new
-<<<<<<< HEAD
     @taxonomies = get_taxonomies.map{ |tax| {id: tax.id.to_s, text: tax.title }}
-=======
-    user_visible_taxonomies = get_taxonomies(current_user.town)
-    @taxonomies = []
-    user_visible_taxonomies.each { |taxonomy| @taxonomies << {id: taxonomy.id.to_s,text: taxonomy.title }}
-
-    @budget_file = generate_budget_file(nil, nil)
->>>>>>> d60be125a12ea90c96c673c7fd46015dccfe4893
   end
 
   # POST /revenues
   # POST /revenues.json
 
   def create
-<<<<<<< HEAD
     @town = Town.find(params[:town])
-=======
-    @town_title = params['town_select'].blank? ? current_user.town : Town.find(params['town_select']).to_s
-    @town = Town.get_town_by_title(@town_title).first
->>>>>>> d60be125a12ea90c96c673c7fd46015dccfe4893
 
     process_files = -> (files) do
       def process_single_file uploaded
@@ -122,18 +113,25 @@ class BudgetFilesController < ApplicationController
       format.json { render :show, status: :created, location: @budget_file }
     end
 
-  # rescue Ole::Storage::FormatError
-  #   message = [t('invalid_format')]
-  #   message << 'Якщо це xls формат переконайтесь у тому що він не xlsx'
-  #   respond_with_error_message(message)
-  # rescue DBF::Column::NameError
-  #   message = [t('invalid_format')]
-  #   message << 'Допустимі формати .dbf, .xsl, .csv'
-  #   respond_with_error_message(message)
-  # rescue => e
-  #   message = "Не вдалося створити візуалізацію : #{e}"
-  #   respond_with_error_message(message)
+  rescue Ole::Storage::FormatError
+    message = [t('invalid_format')]
+    message << 'Якщо це xls формат переконайтесь у тому що він не xlsx'
+    respond_with_error_message(message)
+  rescue DBF::Column::NameError
+    message = [t('invalid_format')]
+    message << 'Допустимі формати .dbf, .xsl, .csv'
+    respond_with_error_message(message)
+  rescue => e
+    message = "Не вдалося створити візуалізацію : #{e}"
+    respond_with_error_message(message)
 
+  # rescue => e
+  #   logger.error "Не вдалося створити візуалізацію. Перевірте коректність змісту завантаженого файлу => #{e}"
+  #
+  #   respond_to do |format|
+  #     format.html { render :new }
+  #     format.json { render json: e, status: :unprocessable_entity }
+  #   end
   end
 
   def respond_with_error_message(message)
@@ -195,14 +193,10 @@ class BudgetFilesController < ApplicationController
     uploaded_io.original_filename
   end
 
-<<<<<<< HEAD
   def generate_budget_file
     @budget_file = BudgetFile.new
     @budget_file.author_model = current_user
   end
-
-=======
->>>>>>> d60be125a12ea90c96c673c7fd46015dccfe4893
   private
 
   def fill_budget_file(data_type,file_path,taxonomy)
@@ -224,20 +218,11 @@ class BudgetFilesController < ApplicationController
     @budget_file.name = @file_name if @budget_file.name.nil?
   end
 
-<<<<<<< HEAD
   def set_taxonomy_by_budget_file(taxonomy_id)
     taxonomy = Taxonomy.where(id: taxonomy_id).first
     if taxonomy.nil?
       taxonomy = create_taxonomy
 
-=======
-  def generate_budget_file taxonomy, file_name
-  end
-
-  def set_taxonomy_by_budget_file(taxonomy_id, file_name)
-    if taxonomy_id.blank?
-      taxonomy = create_taxonomy params[:area], file_name
->>>>>>> d60be125a12ea90c96c673c7fd46015dccfe4893
       taxonomy.town = @town
       taxonomy
     else
