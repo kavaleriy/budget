c735fb727de30d5af1d806d97dee9863dec7e4b9
Sorey <3sorey4@gmail.com>
2016-12-07 12:19:13 +0200

    add town_id for create budget_files

--- app/controllers/budget_file_rovs_controller.rb
+++ app/controllers/budget_file_rovs_controller.rb
@@ -26,6 +26,7 @@ class BudgetFileRovsController < BudgetFilesController
 
     taxonomy.title = get_file_title
     taxonomy.area = area_id
+    taxonomy.town = Town.get_user_town(current_user)
 
     taxonomy
   end
