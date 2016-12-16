15122c401b69e64f8cf0bd1a8d226c280d475cfd
kavaleriy <begemot.lery@mail.ru>
2016-12-16 05:05:15 -0800

    fix taxonomy kvk dictionary

--- app/models/taxonomy_rov.rb
+++ app/models/taxonomy_rov.rb
@@ -16,11 +16,11 @@ class TaxonomyRov < Taxonomy
     else
       {
           # 'fond'=>{:level => 1, :title=> I18n.t('mongoid.taxonomy_rot.fond')},
-          # 'kvk' =>{:level => 2, :title=>I18n.t('mongoid.taxonomy_rov.department')},
           'ktfk_aaa'=>{:level => 1, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.func_code_aaa')},
           'ktfk'=>{:level => 2, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.func_code')},
           'kekv' =>{:level => 3, :draggable => true, :title=>I18n.t('mongoid.taxonomy_rov.economy')},
           'krk'=>{:level => 4, :title=>I18n.t('mongoid.taxonomy_rov.disposer')},
+          'kvk' =>{:level => 5, :title=>I18n.t('mongoid.taxonomy_rov.department')},
       }
     end
   end
