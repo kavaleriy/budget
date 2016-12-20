81ecf059e77d2acb82feb12debea3c2fa7a6fd6d
Sorey <3sorey4@gmail.com>
2016-12-20 18:10:45 +0200

    fix indicators partial, first setting get visualisations by ton

--- presenters/export_budget/form_presenter.rb
+++ presenters/export_budget/form_presenter.rb
@@ -1,25 +1,43 @@
 class ExportBudget::FormPresenter
 
-  def initialize(user_id,locale)
-    @user = User.find(user_id)
+  # def initialize(user_id,locale)
+  #   @user = User.find(user_id)
+  #   @locale = locale
+  # end
+  #
+  # def taxonomies_rot
+  #   TaxonomyRot.visible_to(@user).to_a
+  # end
+  #
+  # def taxonomies_rov
+  #   TaxonomyRov.visible_to(@user).to_a
+  # end
+  #
+  # def calendars
+  #   Calendar.visible_to(@user,@locale).to_a
+  # end
+  #
+  # def indicates
+  #   Indicate::Taxonomy.visible_to(@user).to_a
+  # end
+
+
+  def initialize(town_id,locale)
+    @town = town_id
     @locale = locale
   end
 
   def taxonomies_rot
-    TaxonomyRot.visible_to(@user).to_a
+    TaxonomyRot.by_town_id(@town).to_a
   end
-
   def taxonomies_rov
-    TaxonomyRov.visible_to(@user).to_a
+    TaxonomyRov.by_town_id(@town).to_a
   end
-
   def calendars
-    Calendar.visible_to(@user,@locale).to_a
+    Calendar.get_calendar_by_town(@town).to_a #.get_calendars_by_locale(@locale).to_a
   end
-
   def indicates
-    Indicate::Taxonomy.visible_to(@user).to_a
+    Indicate::Taxonomy.get_indicate_by_town(@town).to_a
   end
-
 
 end
