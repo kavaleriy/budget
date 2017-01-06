223f9d5226a65a43dff0b4cc905f10eb9752ceb0
Sorey <3sorey4@gmail.com>
2017-01-06 17:06:37 +0200

    fix create free budget_file

--- app/controllers/budget_file_frees_controller.rb
+++ app/controllers/budget_file_frees_controller.rb
@@ -1,16 +1,24 @@
 class BudgetFileFreesController < BudgetFilesController
+  include BudgetFileTerra
+  before_action :set_areas, only: [:new]
 
   protected
 
-  def generate_budget_file
-    @budget_file = BudgetFileFree.new
+  def generate_budget_file taxonomy, file_name
+    if taxonomy
+      budget_file = BudgetFileFree.find_or_create_by(taxonomy: taxonomy, name: file_name)
+    else
+      budget_file = BudgetFileFree.new
+    end
+
+    budget_file
   end
 
   def get_taxonomies
     TaxonomyFree.by_town(current_user.town_model)
   end
 
-  def create_taxonomy
+  def create_taxonomy n, m    # (n, m) for successful create
     TaxonomyFree.create!(:owner => @town_title)
   end
 
