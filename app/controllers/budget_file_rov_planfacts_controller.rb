5f77f7fef7dbd076dd9bb4bc0042ef6122098ece
Begemot <begemot.valery@gmail.com>
2017-01-16 13:31:01 +0200

    fix plan-fact BudgetFiles

--- app/controllers/budget_file_rov_planfacts_controller.rb
+++ app/controllers/budget_file_rov_planfacts_controller.rb
@@ -2,8 +2,14 @@ class BudgetFileRovPlanfactsController < BudgetFileRovsController
 
   protected
 
-  def generate_budget_file
-    @budget_file = BudgetFileRovPlanfact.new
+  def generate_budget_file taxonomy, file_name
+    if taxonomy
+      budget_file = BudgetFileRovPlanfact.find_or_create_by(taxonomy: taxonomy, name: file_name)
+    else
+      budget_file = BudgetFileRovPlanfact.new
+    end
+
+    budget_file
   end
 
 end
