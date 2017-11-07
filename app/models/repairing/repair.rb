88df7123f46013a8db20ef860528d80b41c5e5d1
Sorey <3sorey4@gmail.com>
2017-11-07 12:11:26 +0200

    fix css list repair photos, move photo model in other file

--- app/models/repairing/repair.rb
+++ app/models/repairing/repair.rb
@@ -200,25 +200,4 @@ module Repairing
     end
   end
 
-  class Photo
-    require 'file_size_validator'
-
-    include Mongoid::Document
-    require 'carrierwave/mongoid'
-    include Mongoid::Timestamps
-
-    embedded_in :repairing_repair, class_name: 'Repairing::Repair'
-
-    mount_uploader :image, Repairing::RepairPhotoUploader
-
-    validates :image, presence: true
-
-    # Not used now, because uploader not saved original image
-    # file_size = 1
-    # validates :image,
-    #           file_size: {
-    #             maximum: file_size.megabytes.to_i,
-    #             message: I18n.t('mongoid.errors.messages.error_max_file_size', file_size: file_size)
-    #           }
-  end
 end
