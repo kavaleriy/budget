29a9d577f86d68353c35f6257e171a5e5655288f
Sorey <3sorey4@gmail.com>
2018-01-12 15:07:38 +0200

    return logic funds_managers

--- app/models/funds_manager.rb
+++ app/models/funds_manager.rb
@@ -57,7 +57,7 @@ class FundsManager
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename).upcase
       when '.CSV'  then Roo::CSV.new(file.path)
-      when '.XLS'  then Roo::Spreadsheet.open(file.path, extension: :xls)
+      when '.XLS'  then Roo::Excel.new(file.path)
       when '.XLSX' then Roo::Excelx.new(file.path, file_warning: :ignore)
       else raise "Unknown file type: #{file.original_filename}"
     end
