e039626f8a9e61e004540ee715d6bcb979fc04af
Sorey <3sorey4@gmail.com>
2018-02-09 19:12:29 +0200

    start build analysis chart two years

--- app/models/municipal/enterprise_file.rb
+++ app/models/municipal/enterprise_file.rb
@@ -57,25 +57,84 @@ module Municipal
     def self.analysis_chart(enterprise_id, code)
       return {} if code.blank?
       file_type = code[2].eql?(FORM_1) ? FORM_1 : FORM_2
-      files = where(enterprise: enterprise_id, file_type: file_type)
+      # files = where(enterprise: enterprise_id, file_type: file_type).order(year: :asc)
+      files = where(enterprise: enterprise_id, :file_type.in => [FORM_1, FORM_2]).order(year: :asc)
       desc = Municipal::CodeDescription.where(code: code).first.try(:description)
       chart = {}
       chart[code] = { years: {}, desc: desc }
 
       code_info = get_code_info(code)
 
-      files.each do |file|
-        year = file.year
-        # chart[code][:years]["c_#{year}"] = {} if chart[code][:years]["c_#{year}"].nil?
+      years = { years: {} }
 
-        code_info['f_codes'].each do |code_f|
-          value_f = file.code_values.where(code: code_f).first.try(:value)
-          # chart[code][:years]["c_#{year}"]["c_#{code_f}"] = value_f
-          instance_variable_set("@c_#{code_f}", value_f.to_f)
+      files.each do |file|
+        before_year = nil
+        year = file.year
+        # chart[code][:years]["c_#{year}"] = {} if chart[code][:years]["c_#{year}"].nil? #########
+        years[:years]["c_#{year}"] = {} if years[:years]["c_#{year}"].nil? #########
+
+        # code_info['f_codes'].each do |code_f|
+        #   value_f = file.code_values.where(code: code_f).first.try(:value)
+        #   # chart[code][:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+        #   instance_variable_set("@c_#{code_f}", value_f.to_f)
+        # end
+
+        if code[1].eql?('1')
+          code_info['f_codes'].each do |code_f|
+            value_f = file.code_values.where(code: code_f).first.try(:value)
+            # chart[code][:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+            years[:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+
+            before_year = years[:years]["c_#{year.to_i - 1}"]
+            # chart[code][:years]["c_#{year}"]["svg_#{code_f}"] = before_year["c_#{code_f}"] + value_f / 2 if before_year #########
+            binding.pry
+            if before_year
+              value_f2 = before_year["c_#{code_f}"] + value_f / 2 #########
+              instance_variable_set("@c_#{code_f}", value_f2.to_f) if value_f2
+            end
+          end
+        else
+          if code[2].eql?('3') # 72301 >
+
+          elsif code[2].eql?('5') # 72501 >
+            if file.file_type.eql?('1')
+              code_info['f_codes'].each do |code_f|
+                if code_f[0].eql?('1')
+                  value_f = file.code_values.where(code: code_f).first.try(:value)
+                  # chart[code][:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+                  years[:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+
+                  before_year = years[:years]["c_#{year.to_i - 1}"]
+
+                  # binding.pry
+                  # chart[code][:years]["c_#{year}"]["svg_#{code_f}"] = before_year["c_#{code_f}"] + value_f / 2 if before_year #########
+                  # binding.pry
+                  if before_year
+                    value_1 = before_year["c_#{code_f}"] #########
+                    value_2 = value_f #########
+                    instance_variable_set("@c1_#{code_f}", value_1.to_f) if value_1
+                    instance_variable_set("@c2_#{code_f}", value_2.to_f) if value_2
+                  end
+                else
+                  if before_year
+                    value_f = file.code_values.where(code: code_f).first.try(:value)
+                    # chart[code][:years]["c_#{year}"]["c_#{code_f}"] = value_f #########
+                    instance_variable_set("@c_#{code_f}", value_f.to_f)
+                  end
+                end
+              end
+
+            else
+
+            end
+
+          end
+
         end
 
-        # example code_info['formula'] = "(c_2350/c_2000)*100"
-        chart[code][:years][year] = eval(code_info['formula']).try(:round, 2)
+        binding.pry
+        # Example code_info['formula'] = "(@c_2350/@c_2000)*100"
+        chart[code][:years][year] = eval(code_info['formula']).try(:round, 2) if before_year
       end
 
       binding.pry
