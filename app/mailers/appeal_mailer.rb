83e4ad6ae35798a8773d766b1fcb960a0f6916bf
Sorey <3sorey4@gmail.com>
2018-03-30 19:18:29 +0300

    send appeal as pdf

--- app/mailers/appeal_mailer.rb
+++ app/mailers/appeal_mailer.rb
@@ -3,8 +3,17 @@ class AppealMailer < ApplicationMailer
 
   def send_appeal(appeal)
     @repairing_appeal = appeal
-
     recipients = @repairing_appeal.recipients.join(",")
+
+    attachments["Ремонт_#{@repairing_appeal.id}.pdf"] =
+      WickedPdf.new.pdf_from_string(
+  render_to_string(
+          pdf: 'send_appeal',
+          template: 'appeal_mailer/send_appeal',
+          layout: 'mailer'
+        )
+      )
+
     mail(to: recipients, subject: 'Звернення.')
   end
 end
