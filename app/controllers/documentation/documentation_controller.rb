e0330776938e7cc5d146faa4abc3069857c51ace
Sorey <3sorey4@gmail.com>
2018-04-17 22:33:50 +0300

    setting view documentation in admin

--- app/controllers/documentation/documentation_controller.rb
+++ app/controllers/documentation/documentation_controller.rb
@@ -7,7 +7,7 @@ module Documentation
 
     def access_user
       return if current_user && current_user.has_any_role?(:admin, :city_authority)
-      redirect_to :back, alert: t('export_budgets.notice_access')
+      redirect_to :root, alert: t('export_budgets.notice_access')
     end
   end
 end
