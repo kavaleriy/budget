53f79baeac87e40e11e2f8d26d5658656af99ccf
Sorey <3sorey4@gmail.com>
2018-06-19 10:02:55 +0300

    change count days and fix name function

--- lib/external_api.rb
+++ lib/external_api.rb
@@ -21,8 +21,8 @@ class ExternalApi
       dates = [today]
 
       while first_date < dates.last do
-        dates << dates.last.days_since(-91)
-        request = e_data_payments_1(payer_erdpou, recipt_edrpou, dates.last.days_since(1), dates[-2])
+        dates << dates.last.days_since(-90)
+        request = e_data_payments_request(payer_erdpou, recipt_edrpou, dates.last.days_since(1), dates[-2])
         transactions += request if request.present?
       end
     else
