f443664f34812e7a9c7fc018b818cfe233e37584
Sorey <3sorey4@gmail.com>
2018-06-20 15:41:55 +0300

    text editor for appeals scenarios and add export link for repair transactions

--- app/models/repairing/appeal_scenario.rb
+++ app/models/repairing/appeal_scenario.rb
@@ -1,6 +1,7 @@
 module Repairing
   class AppealScenario
     include Mongoid::Document
+    include Rails.application.routes.url_helpers
     field :title, type: String
     field :text_form, type: String
     field :text_appeal, type: String
