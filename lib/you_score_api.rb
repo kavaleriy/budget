6776444073b1db1f2c69c09284e0b3d3673cbd80
Sorey <3sorey4@gmail.com>
2018-07-02 17:47:52 +0300

    new structure score data

--- lib/you_score_api.rb
+++ lib/you_score_api.rb
@@ -1,20 +1,24 @@
 class YouScoreApi
   def self.set_tax_debt
     enterprises = Municipal::Enterprise.all
-    enterprises.each do |enterprise|
+
+    data = []
+    enterprises.each_with_index do |enterprise, i|
       tax_debt = ExternalApi.tax_debt(enterprise.edrpou)
+      # @edr_data_bot = ExternalApi.data_bot_edr(enterprise.edrpou).first
+      # tax_debt = @edr_data_bot && @edr_data_bot.try(:key?, 'full_name') ? @edr_data_bot : { message: 'no data'}
 
-      edrpou_hash = { edrpou: enterprise.edrpou }
-      tax_debt.merge(edrpou_hash)
-      json = File.read('public/group_debt.json')
-
-      File.open('public/group_debt.json', 'w') do |f|
-        f.puts JSON.pretty_generate(JSON.parse(json) << tax_debt)
-      end
+      edrpou_hash = { edrpou: enterprise.edrpou, enterprise_id: enterprise.id.to_s}
+      data << tax_debt.merge(edrpou_hash)
 
       enterprise.debt = tax_debt['debt']
       enterprise.actual_date = tax_debt['actualDate']
       enterprise.save
+    end
+
+    file_path = Rails.root.join('public', 'group_debt.json')
+    File.open(file_path, 'w') do |f|
+      f.puts JSON.pretty_generate(data)
     end
   end
 
@@ -28,26 +32,17 @@ class YouScoreApi
   def self.set_financial_scoring(id, edrpou)
     scoring_years = ExternalApi.financial_scoring(edrpou)
 
+    # company_data = ExternalApi.data_bot_decisions(edrpou)
+    # scoring_years = company_data['items'].present? ? company_data['items'] : { 'message' => 'no data'}
+
+    ent_data = { edrpou: edrpou, enterprise_id: id.to_s, scores: [] }
+    # scoring_years[0..2].each do |scoring_year|
     scoring_years.each do |scoring_year|
-      binding.pry
-      edrpou_hash = { edrpou: edrpou, year: scoring_year[:year]}
-      scoring_year.merge(edrpou_hash)
-      json = File.read('public/group.json')
+      scoring_data = ExternalApi.financial_scoring_per_year(edrpou, scoring_year['year'])
+      data = scoring_data.merge({ year: scoring_year['year'] })
+      ent_data[:scores] << data
 
-      File.open('public/group.json', 'w') do |f|
-        f.puts JSON.pretty_generate(JSON.parse(json) << scoring_year)
-      end
-
-      scoring_data = ExternalApi.financial_scoring_per_year(edrpou, scoring_year[:year])
-      financial_scores = YouScore::FinancialScore.find_or_initialize_by(enterprise: id)
-
-      scoring_data.merge(edrpou_hash)
-      json = File.read('public/group.json')
-
-      File.open('public/group_scores.json', 'w') do |f|
-        f.puts JSON.pretty_generate(JSON.parse(json) << edrpou_hash)
-      end
-
+      financial_scores = YouScore::FinancialScore.find_or_initialize_by(enterprise: id, year: scoring_year['year'])
       financial_scores.enterprise = id
       financial_scores.year = scoring_year['year']
       financial_scores.score_mark = scoring_data['score']['mark']
@@ -64,6 +59,13 @@ class YouScoreApi
       binding.pry
       financial_scores.save
     end
+
+    group_file_path = Rails.root.join('public', 'group.json')
+    json = File.read(group_file_path)
+    File.open(group_file_path, 'w') do |f|
+      f.puts JSON.pretty_generate(JSON.parse(json) << ent_data)
+    end
+
   end
 
 end
