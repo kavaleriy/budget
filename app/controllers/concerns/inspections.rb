1f4570ea32a1b26f33acf10c84ac8a21f9ef3eba
Sorey <3sorey4@gmail.com>
2018-09-17 16:12:04 +0300

    fix table view and change structure inspection

--- app/controllers/concerns/inspections.rb
+++ app/controllers/concerns/inspections.rb
@@ -11,25 +11,15 @@ module Inspections
           'regulator' => item['regulator'],
           'activity_type' => item['data'].try(:[], 'activity_type') || item['plan'].try(:[], 'activity_type'),
           'risk' => item['data'].try(:[], 'risk') || item['plan'].try(:[], 'risk'),
-          'status' => status(item),
+          'status' => item['data'].try(:[], 'status') || item['plan'].try(:[], 'status'),
           'date_finish' =>  item['data'].try(:[], 'date_finish') || '',
-          'sanction_sum' => calculate_sum(item).to_s || '',
-          'reason_pdf' => document_link?(item) ? "http://cdn.inspections.gov.ua/#{item['internal_id']}/reason.pdf" : '',
-          'result_act_pdf' => document_link?(item) ?  "http://cdn.inspections.gov.ua/#{item['internal_id']}/result_act.pdf" : ''
+          'sanction_sum' => calculate_sum(item).to_s || ''
         }
 
       inspections_arr.push(inspection)
     end
 
     inspections_arr
-  end
-
-  def document_link?(item)
-    status(item).eql?('Проведено')
-  end
-
-  def status(item)
-    item['data'].try(:[], 'status') || item['plan'].try(:[], 'status')
   end
 
   def calculate_sum(item)
