068275e64730b40815f6f8ef6f7c421c90ff6b70
odialko <viktor1532@gmail.com>
2019-01-16 17:50:38 +0200

    search city

--- app/models/town.rb
+++ app/models/town.rb
@@ -89,13 +89,16 @@ class Town
     #                                                      User.mask_for(:state_enterprise)]).pluck(:town_model_id)
 
     # scope users with multiple role also
-    city_authority_users = User.all.map{|u| u.town_model_id if u.has_any_role?([:city_authority,
-                                                                       :central_authority,
-                                                                       :municipal_enterprise,
-                                                                       :state_enterprise])}.compact
+    city_authority_users = User.all.map do |u|
+      if u.has_any_role?([:city_authority, :central_authority, :municipal_enterprise, :state_enterprise])
+        u.town_model_id
+      else
+        nil
+      end
+    end
 
     # second we find all they towns and last add regular expression to all they towns
-    Town.where(:_id.in => city_authority_users).and(title: Regexp.new("^#{query}.*"))
+    Town.where(:_id.in => city_authority_users.compact).and(title: Regexp.new("^#{query}.*"))
   end
 
   def is_test?
