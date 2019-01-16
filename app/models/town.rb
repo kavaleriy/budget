6cc0a5c6bdf9a9b6be40ccdfbdd03444c3a198bd
odialko <viktor1532@gmail.com>
2019-01-16 17:58:37 +0200

    search city delete cache

--- app/models/town.rb
+++ app/models/town.rb
@@ -92,14 +92,10 @@ class Town
     city_authority_users = User.all.map do |u|
       if u.has_any_role?([:city_authority, :central_authority, :municipal_enterprise, :state_enterprise])
         u.town_model_id
-      else
-        nil
       end
     end
     # second we find all they towns and last add regular expression to all they towns
     Town.where(:_id.in => city_authority_users.compact).and(title: Regexp.new("^#{query}.*"))
-
-
   end
 
   def is_test?
