70fc052d56367d2dacde774a7d8dc6827d15c56b
odialko <viktor1532@gmail.com>
2019-01-21 12:30:26 +0200

    binding.pry delete

--- lib/charts/analysis_chart.rb
+++ lib/charts/analysis_chart.rb
@@ -32,25 +32,28 @@ module Charts
       }
 
       years = {}
+
       files.each do |file|
         year = file.year
         years[year] = {} unless years[year]
 
-        code_formula_info['f_codes'].each do |code_f|
-          # TODO: Put all formules to classes imediatly!!!
+        if code_formula_info['f_codes'].present?
+          code_formula_info['f_codes'].each do |code_f|
+            # TODO: Put all formules to classes imediatly!!!
 
-          value_f = file.code_values.where(code: code_f).first.try(:value)
+            value_f = file.code_values.where(code: code_f).first.try(:value)
 
-          if code_f == '2350' && value_f == 0
-            code_f = '2355'
-            value_f = file.code_values.where(code: code_f).first.try(:value)
-          elsif code_f == '2190' && value_f == 0
-            code_f = '2195'
-            value_f = file.code_values.where(code: code_f).first.try(:value)
+            if code_f == '2350' && value_f == 0
+              code_f = '2355'
+              value_f = file.code_values.where(code: code_f).first.try(:value)
+            elsif code_f == '2190' && value_f == 0
+              code_f = '2195'
+              value_f = file.code_values.where(code: code_f).first.try(:value)
+            end
+
+            years[year][code_f] = value_f if value_f
+            # {2015=>{"1495"=>5353}, 2016=>{"1495"=>5353, "2350"=>71}, 2017=>{"1495"=>5296, "2350"=>283}}
           end
-
-          years[year][code_f] = value_f if value_f
-          # {2015=>{"1495"=>5353}, 2016=>{"1495"=>5353, "2350"=>71}, 2017=>{"1495"=>5296, "2350"=>283}}
         end
       end
 
@@ -67,7 +70,7 @@ module Charts
           instance_variable_set("@c2_#{code_f}", before_year[code_f].to_f)
         end if code_formula_info['codes_2_year'].present?
 
-        chart[code][:years][year_k] = eval(code_formula_info['formula']).try(:round, 3) if before_year || !code_formula_info['codes_2_year'].present?
+        chart[code][:years][year_k] = eval(code_formula_info['formula']).try(:round, 3) if before_year || code_formula_info['codes_2_year'].present?
       end
 
       chart
@@ -80,14 +83,8 @@ module Charts
 
       code_info = {}
       csv.each do |row|
-        # binding.pry
         next unless row['code'].eql?(code)
-
-
         code_info['formula'] = row['formula']
-
-
-
         code_info['abbreviation'] = row['abbreviation']
         code_info['codes_1_year'] = row['codes_1_year'].split('|').reject(&:blank?) if row['codes_1_year']
         code_info['codes_2_year'] = row['codes_2_year'].split('|').reject(&:blank?) if row['codes_2_year']
