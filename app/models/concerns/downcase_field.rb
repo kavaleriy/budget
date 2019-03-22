39184946b66625ef8fee0f766529d684f6fadbd1
odialko <viktor1532@gmail.com>
2019-03-22 12:46:15 +0200

    issues

--- app/models/concerns/downcase_field.rb
+++ app/models/concerns/downcase_field.rb
@@ -2,8 +2,11 @@ module DowncaseField
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def downcase_str(str)
+    def downcase_gsub_str(str)
       str.mb_chars.downcase.gsub(/[^a-zA-ZА-Яа-я\-]/,"").to_s
+    end
+    def downcase_str(str)
+      str.mb_chars.downcase.to_s
     end
   end
 end
--- app/models/concerns/status_btn.rb
+++ app/models/concerns/status_btn.rb
@@ -3,13 +3,12 @@ module StatusBtn
 
   module ClassMethods
     def status_btn(status)
-      case status
-        when 'оренда'
-          'rent'
-        when 'в користуванні'
-          'occupied'
-        else
-          'free'
+      if status == 'оренда'
+        'rent'
+      elsif status == 'в користуванні'
+        'occupied'
+      else
+        'free'
       end
     end
   end
