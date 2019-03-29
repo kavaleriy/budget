cbb911c1d800bfc1508ee77d2274182391e54e94
Alexandr <1989alpan@gmail.com>
2019-03-29 11:53:23 +0200

    build tree

--- config/routes.rb
+++ config/routes.rb
@@ -371,8 +371,8 @@ Rails.application.routes.draw do
 
     get 'visify/visify/:file_id' => 'visify#visify'
     get 'visify/type/:file_id/:type' => 'visify#type', as: 'visify_type'
-    get 'visify/get_bubbletree_data/:file_id' => 'visify#get_bubbletree_data'
-    get 'visify/get_bubbletree_data/:file_id/:levels' => 'visify#get_bubbletree_data'
+    get 'visify/get_bubbletree_data/:file_id' => 'gov_visify#index'
+    get 'visify/get_bubbletree_data/:file_id/:levels' => 'gov_visify#index'
     get 'gov_visify/get_bubbletree_data/:file_id/:levels' => 'gov_visify#index'
     get 'visify/get_bubblesubtree/:file_id/:taxonomy/:key' => 'visify#get_bubblesubtree'
     get 'visify/get_bubblesubtree_with_fact/:file_id/:taxonomy/:key' => 'visify#get_bubblesubtree_with_fact'
--- lib/budget/files/builders/tree.rb
+++ lib/budget/files/builders/tree.rb
@@ -4,6 +4,7 @@ module Budget
       class Tree
 
         def initialize(files, column)
+          @year = '2019'
           @files = files.to_a
           @column = column
           @res = build_scelleton
@@ -11,17 +12,18 @@ module Budget
         end
 
         def call
-          
           @files.group_by{|a| a.month}.each do |month, values|
-            @res[:amount][:plan]['2018'][month] = {
-              total: values.sum{|val| val.budget_plan.to_f},
+            @res[:amount][:plan][@year][month] = {
+              # total: values.sum{|val| val.budget_plan.to_f},
+              total: 1,
               fonds:{
                 '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.budget_plan.to_f},
                 '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.budget_plan.to_f}
               }
             } 
-            @res[:amount][:fact]['2018'][month] = {
-              total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.total_done.to_f},
+            @res[:amount][:fact][@year][month] = {
+              total: 1,
+              # total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.total_done.to_f},
               fonds:{
                 '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.total_done.to_f},
                 '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.total_done.to_f}
@@ -32,10 +34,11 @@ module Budget
           @res[:key] = 'Всього'
           @res[:icon] = '/assets/icons/pig.svg'
           @res[:color] = 'green'
-          @res[:children] = build_tree(@files)
+          @files.select{|file| file.public_send(@column) =~ level_regex(0)[:regex]}.group_by{|file| file.public_send(@column)}.each do |key, files|
+            @res[:children] << build_tree(files, 1, key)
+          end
           @res
         end
-
 
         private 
 
@@ -43,61 +46,73 @@ module Budget
           {
             amount: {
               fact: {
-                '2018' => {}
+                @year => {}
               },
               plan: {
-                '2018' => {}
+                @year => {}
               }
             },
             label: '',
             key: '',
-            children: []
+            children: [],
+            taxonomy: 'kkd_a'
           }
         end
 
-        def build_tree(grouped_files, level = 0, file_key = '')
-          if level <= 1
+
+
+        def fill_amount_data(grouped_files_by_month)
+          tmp = build_scelleton
+          grouped_files_by_month.each do |month, values|
+            tmp[:amount][:plan][@year][month] = {
+              total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.budget_plan.to_f},
+              fonds:{
+                '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.budget_plan.to_f},
+                '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.budget_plan.to_f}
+              }
+            } 
+            tmp[:amount][:fact][@year][month] = {
+              total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.total_done.to_f},
+              fonds:{
+                '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.total_done.to_f},
+                '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.total_done.to_f}
+              }
+            } 
+          end
+          tmp
+        end
+
+        def level_regex(level, file_key='')
+          if level == 0
+            regex = '[0]{7}$'
+          elsif level == 1
             regex = file_key.blank? ? '[0]{7}$' : "[0]{#{file_key.size - level - 1}}$"
             level += 1
           else
             regex = "[0]{#{file_key.size - (level * 2)}}$"
             level += 2
-            # binding.pry
           end
-          regex = Regexp.new(regex)
-          tree = []
-          grouped_files.select{|file| file.public_send(@column) =~ regex}.group_by{|file| file.public_send(@column)}.each do |key, grouped_by_column|
+          { level: level, regex: Regexp.new(regex) }
+        end
+
+        def build_tree(grouped_files, level = 0, file_key = '')
+          regex = level_regex(level)
+          tmp = {}
+          grouped_files.group_by{|file| file.public_send(@column)}.each do |key, grouped_by_column|
             file_key = key
-            grouped_files_by_month = grouped_by_column.group_by{|a| a.month}
-            tmp = build_scelleton
-            grouped_files_by_month.each do |month, values|
-              tmp[:amount][:plan]['2018'][month] = {
-                total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.budget_plan.to_f},
-                fonds:{
-                  '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.budget_plan.to_f},
-                  '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.budget_plan.to_f}
-                }
-              } 
-              tmp[:amount][:fact]['2018'][month] = {
-                total: values.select{|val| val.fund_type == 'TOTAL'}.sum{|val| val.total_done.to_f},
-                fonds:{
-                  '1' => values.select{|val| val.fund_type == 'COMMON'}.sum{|val| val.total_done.to_f},
-                  '2' => values.select{|val| val.fund_type == 'SPECIAL'}.sum{|val| val.total_done.to_f}
-                }
-              } 
-              
-            end
+            tmp = fill_amount_data(grouped_by_column.group_by{|a| a.month})
             tmp[:label] = grouped_by_column.first.income_code_name
             tmp[:key] = key
             child_regex = Regexp.new("[0]{#{file_key.size - (level * 2)}}$")
+            child_tmp = []
             children = @grouped_by_income_files.keys.select{|file| file.start_with?(file_key[0...level]) && file =~ child_regex && file != file_key}
             children.each do |child_key|
-              tmp[:children] << build_tree(@grouped_by_income_files[child_key], level, child_key)
+              child = build_tree(@grouped_by_income_files[child_key], regex[:level], child_key)
+              child_tmp << child
             end
-
-            tree << tmp
+            tmp[:children] = child_tmp
           end
-          tree
+          tmp
         end
 
       end
